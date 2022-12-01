778cd0695340e936399c2c9eb5d6bd923a20f86e
ym <47711835+ym496@users.noreply.github.com>
2022-12-02 00:21:00 +0530

    Add files via upload

---
+++ aoc_sol/day1.py
@@ -0,0 +1,17 @@
+fdescriptor = open('aoc_input/day.txt', 'r')
+maxCalories = 0
+elf = []
+Elves = []
+for i in fdescriptor:
+  if i == '\n':
+    Calories = sum(elf)
+    Elves.append(Calories)
+    if Calories > maxCalories:
+      maxCalories = Calories
+    elf.clear()
+
+  else:
+    elf.append(int(i.rstrip()))
+
+Elves.sort(reverse=True)
+TotalTopthree = Elves[0] + Elves[1] + Elves[2]
